471a79cb00a759687615d0586598309b7dd7c8ff
Kaushik Rishi <rishi.cp01@gmail.com>
2020-11-23 18:29:50 +0530

    Added FISH SHELL Conf files

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+set -g -x fish_greeting ''
+
+# alias for windows c drive
+alias win-c-dr="cd /media/darkangel/Windows/"
+
+# alias for windows d drive
+alias win-d-dr="cd /media/darkangel/Data/resources/"
+
+# alias for nautilius file manager nautilus .
+alias fm="nautilus . &"
+
+# alias for myworkspce
+alias msp="cd ~/Desktop/My_Workspace"
+
+# alias for thunar file manager
+alias th="thunar"
+
+# alias for ls
+# alias ls="ls -lah"
+
+# do something for the danger commands
+# rm -rf /*
+
+alias xop="xdg-open"
+
+alias rishi_sql="mysql -u rishi -p"
+
+# alias for opening applications directory
+alias app_images="~/Apps/ && thunar . && exit"
+
+alias yt-dl="/usr/bin/python3 /usr/local/bin/youtube-dl"
+alias mp3="yt-dl -x --embed-thumbnail --audio-format mp3"
+
+
